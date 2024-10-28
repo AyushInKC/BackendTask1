1c7ad24d14c07f0b464ce312acd38b93c3d0f78e
AyushInKC <sirfayush1008@gmail.com>
2024-10-28 19:47:52 +0530

    Done with the @Value annotation error

--- blogAPI/blogAPI/src/main/java/com/blogApp/Controllers/fileController.java
+++ blogAPI/blogAPI/src/main/java/com/blogApp/Controllers/fileController.java
@@ -1,15 +1,13 @@
 package com.blogApp.Controllers;
-
 import com.blogApp.Service.fileService;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,25 +15,25 @@ import java.io.InputStream;
 @RestController
 @RequestMapping("/file/")
 public class fileController {
+
   private final fileService fileServices;
-  public fileController(fileService fileService){
-      this.fileServices=fileService;
-  }
   @Value("${project.poster}")
-      private String path;
+  private String path;
+
+  public fileController(fileService fileService){
+    this.fileServices = fileService;
+  }
+
   @PostMapping("/upload/")
-    public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException{
-//      fileServices.uploadFile(path,file);
-      String uploadedFileName= fileServices.uploadFile(path,file);
-      return ResponseEntity.ok("File Uploaded :"+uploadedFileName);
+  public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException {
+    String uploadedFileName = fileServices.uploadFile(path, file);
+    return ResponseEntity.ok("File Uploaded: " + uploadedFileName);
   }
 
   @GetMapping("/{fileName}")
-    public void serveFileHandler(@PathVariable String fileName , HttpServletResponse response) throws FileNotFoundException {
-      InputStream resourceFile=fileServices.getResourceFile(path,fileName);
-      response.setContentType(MediaType.IMAGE_PNG_VALUE);
-
-
+  public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException{
+    InputStream resourceFile = fileServices.getResourceFile(path, fileName);
+    response.setContentType(MediaType.IMAGE_PNG_VALUE);
+    StreamUtils.copy(resourceFile,response.getOutputStream());
   }
-
 }
